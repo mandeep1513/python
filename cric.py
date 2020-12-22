e5b559b8d8e311b97e862f8653dc51f1dcaf2c84
mandeep1513 <mannsingh1513@gmail.com>
2020-12-23 00:41:32 +0530

    new file

---
+++ cric.py
@@ -0,0 +1,12 @@
+import bs4
+import requests
+url = 'https://cricketexchange.in/live'
+data = requests.get(url)
+soup = bs4.BeautifulSoup(data.text, 'html.parser')
+print (soup.prettify())
+for para in soup.find_all ('a'):
+    #print(para.text)
+    for links in soup.find_all ('a'):
+        links = links.get ('href')
+        print(links)
+
