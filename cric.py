fc65ee4038817ce724725884695e32be4465cd29
mandeep1513 <mannsingh1513@gmail.com>
2020-12-23 00:50:19 +0530

    new changes

--- cric.py
+++ cric.py
@@ -5,7 +5,7 @@ data = requests.get(url)
 soup = bs4.BeautifulSoup(data.text, 'html.parser')
 print (soup.prettify())
 for para in soup.find_all ('a'):
-    #print(para.text)
+    print(para.text)
     for links in soup.find_all ('a'):
         links = links.get ('href')
         print(links)
